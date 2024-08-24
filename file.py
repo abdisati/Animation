4f3139477c1c9cf4d988cf68b916771df2241b94
Abdi Deresa <Abdishegute@gmail.com>
2024-08-23 20:09:03 -0700

    added if statement

--- file.py
+++ file.py
@@ -15,9 +15,14 @@ canvas.pack()
 photo=PhotoImage(file="C:\\Users\\abdis\\OneDrive\\Desktop\\UFO.png")
 
 my_image = canvas.create_image(0,0,image=photo, anchor=NW)
+
+image_width = photo.width()
+image_height = photo.height()
 while True:
     coordinates = canvas.coords(my_image)
     print(coordinates)
+    if (coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0):
+        xVelocity=-xVelocity
     canvas.move(my_image,xVelocity,0)
     window.update()
     time.sleep(0.01)
