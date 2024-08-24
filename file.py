942a13d950e3141cb3f01fd7f356949a5c9e1caf
Abdi Deresa <Abdishegute@gmail.com>
2024-08-23 19:21:22 -0700

    added a while loop

--- file.py
+++ file.py
@@ -4,6 +4,9 @@ import time
 WIDTH =500
 HEIGHT =500
 
+xVelocity = 1
+yVelocity =1
+
 window = Tk()
 
 canvas= Canvas(window, width=WIDTH, height=HEIGHT)
@@ -12,6 +15,12 @@ canvas.pack()
 photo=PhotoImage(file="C:\\Users\\abdis\\OneDrive\\Desktop\\UFO.png")
 
 my_image = canvas.create_image(0,0,image=photo, anchor=NW)
+while True:
+    coordinates = canvas.coords(my_image)
+    print(coordinates)
+    canvas.move(my_image,xVelocity,0)
+    window.update()
+    time.sleep(0.01)
 
 
 
