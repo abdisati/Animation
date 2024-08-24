b8d55f67a8881949efa3631375c36c9efaca7f82
Abdi Deresa <Abdishegute@gmail.com>
2024-08-23 20:21:41 -0700

    added a background photo

--- file.py
+++ file.py
@@ -4,13 +4,19 @@ import time
 WIDTH =500
 HEIGHT =500
 
-xVelocity = 1
-yVelocity =1
+xVelocity = 3
+yVelocity =2
 
 window = Tk()
 
 canvas= Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
+
+
+
+background_photo=PhotoImage(file="C:\\Users\\abdis\\OneDrive\\Desktop\\back.png")
+
+background_image = canvas.create_image(0,0,image=background_photo, anchor=NW)
 
 photo=PhotoImage(file="C:\\Users\\abdis\\OneDrive\\Desktop\\UFO.png")
 
@@ -23,7 +29,9 @@ while True:
     print(coordinates)
     if (coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0):
         xVelocity=-xVelocity
-    canvas.move(my_image,xVelocity,0)
+    if (coordinates[1]>=(HEIGHT-image_height) or coordinates[1]<0):
+        yVelocity=-yVelocity
+    canvas.move(my_image,xVelocity,yVelocity)
     window.update()
     time.sleep(0.01)
 
